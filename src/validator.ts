a1334d3a33ef0e72a98119f03ac317074ebc8a2b
ferried <harlancui@outlook.com>
2019-11-27 14:08:03 +0800

    feature(validate): add some regExp

--- src/validator.ts
+++ src/validator.ts
@@ -10,14 +10,15 @@ export enum BrowserType {
     IE9 = 'ie9',
     IE8 = 'ie8,',
     IE7 = 'ie7',
-    QQ_SE = 'qq_se',
+    QQ = 'qq',
     MAXTHON = 'maxthon',
     CHROME = 'chrome',
     QIHU_EE = 'qihu_ee',
     QIHU_SE = 'qihu_se',
     UC = 'ubrowser',
     METASR = 'metasr',
-    FIREFOX = 'firefox'
+    FIREFOX = 'firefox',
+    SAFARI = 'safari'
 }
 
 export interface Validator {
@@ -27,9 +28,96 @@ export interface Validator {
 
 export const validators: Array<Validator> = [
     {
+        name: BrowserType.EDGE, validate: (ua: string, ...args) => {
+            return new RegExp(/edge\/\d*/).test(ua);
+        }
+    },
+    {
         name: BrowserType.IE11, validate: (ua: string, ...args) => {
             return new RegExp(/rv:11.0/).test(ua);
         }
+    },
+    {
+        name: BrowserType.IE10, validate: (ua: string, ...args) => {
+            return new RegExp(/msie\s*10/).test(ua);
+        }
+    },
+    {
+        name: BrowserType.IE9, validate: (ua: string, ...args) => {
+            return new RegExp(/msie\s*9/).test(ua);
+        }
+    },
+    {
+        name: BrowserType.IE8, validate: (ua: string, ...args) => {
+            return new RegExp(/msie\s*8/).test(ua);
+        }
+    },
+    {
+        name: BrowserType.IE7, validate: (ua: string, ...args) => {
+            return new RegExp(/msie\s*7/).test(ua);
+        }
+    },
+    {
+        name: BrowserType.QQ, validate: (ua: string, ...args) => {
+            return new RegExp(/qqbrowser\/\d*/).test(ua);
+        }
+    },
+    {
+        name: BrowserType.MAXTHON, validate: (ua: string, ...args) => {
+            return new RegExp(/maxthon\/\d*/).test(ua);
+        }
+    },
+    {
+        name: BrowserType.CHROME, validate: (ua: string, ...args) => {
+            return new RegExp(/chrome\d*/).test(ua) &&
+                !new RegExp(/qqbrowser\/\d*/).test(ua) &&
+                !new RegExp(/edge/).test(ua) &&
+                !new RegExp(/maxthon\/\d*/).test(ua) &&
+                !new RegExp(/qihu\s*360ee/).test(ua) &&
+                !new RegExp(/qihu\s*360se/).test(ua) &&
+                !new RegExp(/ubrowser\/\d*/).test(ua) &&
+                !new RegExp(/metasr\s*\d*/).test(ua);
+        }
+    },
+    {
+        name: BrowserType.QIHU_EE, validate: (ua: string, ...args) => {
+            return new RegExp(/qihu\s*360ee/).test(ua);
+        }
+    },
+    {
+        name: BrowserType.QIHU_SE, validate: (ua: string, ...args) => {
+            return new RegExp(/qihu\s*360se/).test(ua);
+        }
+    },
+    {
+        name: BrowserType.UC, validate: (ua: string, ...args) => {
+            return new RegExp(/ubrowser\/\d*/).test(ua);
+        }
+    },
+    {
+        name: BrowserType.METASR, validate: (ua: string, ...args) => {
+            return new RegExp(/metasr\s*\d*/).test(ua);
+        }
+    },
+    {
+        name: BrowserType.FIREFOX, validate: (ua: string, ...args) => {
+            return new RegExp(/firefox\/\d*/).test(ua);
+        }
+    },
+    {
+        name: BrowserType.SAFARI, validate: (ua: string, ...args) => {
+            return new RegExp(/safari\/\d*/).test(ua) &&
+                !new RegExp(/metasr\s*\d*/).test(ua) &&
+                !new RegExp(/qqbrowser\/\d*/).test(ua) &&
+                !new RegExp(/maxthon\/\d*/).test(ua) &&
+                !new RegExp(/chrome\d*/).test(ua) &&
+                !new RegExp(/qihu\s*360ee/).test(ua) &&
+                !new RegExp(/qihu\s*360se/).test(ua) &&
+                !new RegExp(/ubrowser\/\d*/).test(ua) &&
+                !new RegExp(/metasr\s*\d*/).test(ua) &&
+                !new RegExp(/firefox\/\d*/).test(ua)
+        }
     }
+
 ];
 
